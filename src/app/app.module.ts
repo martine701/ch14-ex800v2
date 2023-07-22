d0e426e8b23836fc014aa106602ccb2c682c654f
martine701 <allietmartine@gmail.com>
2023-07-22 01:03:01 +0100

    sat commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent, Paragraph } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, Paragraph
   ],
   imports: [
     BrowserModule,
